d71bcdbcfdd63b4e2b0f2681f2602fba434aa2a9
Matilda Mared <matildamared@live.se>
2022-03-03 08:40:20 +0100

    practice working with constructors

---
+++ Section2/Constructors/Order.cs
@@ -0,0 +1,5 @@
+namespace Constructors;
+
+public class Order {
+	
+}
---
+++ Section2/Constructors/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Constructors {
+	class Program {
+		static void Main(string[] args) {
+			var customer = new Customer(1, "John");
+			Console.WriteLine(customer.Id);
+			Console.WriteLine(customer.Name);
+
+			var order = new Order();
+			customer.Orders.Add(order);
+
+			// Object initializer
+			var anotherCustomer = new Customer {
+				Id = 5,
+				Name = "Jane",
+			};
+			
+			Console.WriteLine(anotherCustomer.Id);
+			Console.WriteLine(anotherCustomer.Name);
+		}
+	}
+}
