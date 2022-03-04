dc15cd78399c05e7093405d83e24e15cf2ac5baa
Matilda Mared <matildamared@live.se>
2022-03-04 07:44:54 +0100

    practice with properties

---
+++ Section2/Properties/Program.cs
@@ -0,0 +1,8 @@
+﻿namespace Properties {
+	class Program {
+		public static void Main(string[] args) {
+			var person = new Person(new DateTime(1990, 07, 07));
+			Console.WriteLine(person.Age);
+		}
+	}
+}
