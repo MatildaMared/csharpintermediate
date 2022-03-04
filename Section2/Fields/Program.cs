442f1dc8711c58e2236170e9a92d290c0316b778
Matilda Mared <matildamared@live.se>
2022-03-04 07:11:51 +0100

    practice working with fields

---
+++ Section2/Fields/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Fields {
+	class Program {
+		static void Main(string[] args) {
+			var customer = new Customer(1);
+			customer.Orders.Add(new Order());
+			customer.Orders.Add(new Order());
+			customer.Orders.Add(new Order());
+
+			Console.WriteLine(customer.Orders.Count);
+		}
+	}
+}
