f3d78cf4c4d4398ab8fd6dc8e7cfffdfc0446f88
Matilda Mared <matildamared@live.se>
2022-03-05 23:14:48 +0100

    practice with indexers

---
+++ Section2/Indexers/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace Indexers {
+	class Program {
+		static void Main(string[] args) {
+			var cookie = new HttpCookie();
+			cookie["name"] = "Matilda";
+			Console.WriteLine(cookie["name"]);
+		}
+	}
+}
