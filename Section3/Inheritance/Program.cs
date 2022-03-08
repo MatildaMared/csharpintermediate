ea63fdb7af6b42883faf1f511c3e01c530338196
Matilda Mared <matildamared@live.se>
2022-03-08 13:46:07 +0100

    practice working with inheritance

---
+++ Section3/Inheritance/Program.cs
@@ -0,0 +1,11 @@
+﻿using System.Diagnostics;
+
+namespace Inheritance {
+	class Program {
+		static void Main(string[] args) {
+			var text = new Text();
+			text.Width = 80;
+			text.Copy();
+		}
+	}
+}
