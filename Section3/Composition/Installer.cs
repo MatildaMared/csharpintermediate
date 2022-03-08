9127b4dd7708817f482b0a69899f518f3cd5e230
Matilda Mared <matildamared@live.se>
2022-03-08 14:10:03 +0100

    practice working with composition

---
+++ Section3/Composition/Installer.cs
@@ -0,0 +1,11 @@
+public class Installer {
+	private readonly Logger _logger;
+
+	public Installer(Logger logger) {
+		this._logger = logger;
+	}
+
+	public void Install() {
+		_logger.Log("We are installing the application");;
+	}
+}
---
+++ Section3/Composition/Program.cs
@@ -0,0 +1,8 @@
+﻿var dbMigrator = new DbMigrator(new Logger());
+
+var logger = new Logger();
+var installer = new Installer(logger);
+
+dbMigrator.Migrate();
+
+installer.Install();
