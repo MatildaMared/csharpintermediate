4ef23860cbac72d4b178250e942ff4288c4fbec4
Matilda Mared <matildamared@live.se>
2022-03-08 19:18:03 +0100

    practice with casting

---
+++ Section3/Casting/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Hello, World!");
+
+var text = new Text();
+Shape shape = text;
+
+text.Width = 200;
+shape.Width = 100;
+
+Console.WriteLine(text.Width);
---
+++ Section3/Casting/Text.cs
@@ -0,0 +1,4 @@
+public class Text : Shape {
+	public int FontSize { get; set; }
+	public string FontName { get; set; }
+}
