fae69ac8e191c64c271bee5da045cf8c21824e2e
Matilda Mared <matildamared@live.se>
2022-03-09 08:05:19 +0100

    practice working with abstract classes

---
+++ Section5/AbstractClasses/Program.cs
@@ -0,0 +1,14 @@
+﻿
+var circle = new Circle();
+circle.Draw();
+
+var rectangle = new Rectangle();
+rectangle.Draw();
+
+public class Rectangle : Shape
+{
+    public override void Draw()
+    {
+        Console.WriteLine("Drawing a rectangle");
+    }
+}
---
+++ Section5/AbstractClasses/Shape.cs
@@ -0,0 +1,12 @@
+public abstract class Shape
+{
+    public int Width { get; set; }
+    public int Height { get; set; }
+
+    public abstract void Draw();
+
+    public void Copy()
+    {
+        Console.WriteLine("Copy shape into clipboard.");
+    }
+}
