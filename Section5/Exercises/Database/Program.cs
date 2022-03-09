39b4f345d55b9c48ccef1ef0ec8fda19f4a5fca3
Matilda Mared <matildamared@live.se>
2022-03-09 19:59:22 +0100

    begin working on database exercise

---
+++ Section5/Exercises/Database/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+public class DbConnection
+{
+    public string ConnectionString { get; set; }
+    public TimeSpan Timeout { get; set; }
+
+    public DbConnection(string connectionString)
+    {
+        if (string.IsNullOrWhiteSpace(connectionString))
+        {
+            throw new ArgumentException("Connection string cannot be null or empty");
+        }
+        this.ConnectionString = connectionString;
+    }
+}
