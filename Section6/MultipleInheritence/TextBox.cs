c06eaa3f663ff94c13bceb4e6de72c6b462f8baa
Matilda Mared <matildamared@live.se>
2022-03-10 14:53:25 +0100

    practice with inheritence

---
+++ Section6/MultipleInheritence/TextBox.cs
@@ -0,0 +1,12 @@
+public class TextBox : UiControl, IDraggable, IDroppable
+{
+    public void Drag()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Drop()
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ Section6/MultipleInheritence/UiControl.cs
@@ -0,0 +1,18 @@
+using System.Drawing;
+
+public class UiControl
+{
+    public string Id { get; set; }
+    public Size Size { get; set; }
+    public Position TopLeft { get; set; }
+
+    public virtual void Draw()
+    {
+        
+    }
+
+    public void Focus()
+    {
+        Console.WriteLine("Recieved focus...");
+    }
+}
